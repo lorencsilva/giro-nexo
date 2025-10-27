52fd3416da0339f46b76ceb40aeb9184845847f7
lorencsilva <lorencastrosilva@gmail.com>
2025-10-26 21:22:14 -0300

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>GIRO NEXO</h1>
+      <p>Bem-vindo ao gerador de revista digital GIRO NEXO. Este é um protótipo inicial.</p>
+    </div>
+  );
+}
